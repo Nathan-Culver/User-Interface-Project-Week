680509275d8b1f083203769e1fcafc729d25b729
Nathan <s6nculve@gmail.com>
2019-02-25 20:38:29 -0800

    Updated css/index.css, index.html, js/index.js, less/services-page.less, and less/tabs.less. There is an issue with the tabs. debugging.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+let tracker = "open";
+
+function toggleMenu () {
+  let image = document.querySelector("#menuImg");
+  if (tracker=="open") {
+    image.src = "img/nav-hamburger-close.png";
+    tracker = "close";
+  } else {
+    image.src = "img/nav-hamburger.png";
+    tracker = "open";
+  }
+};
